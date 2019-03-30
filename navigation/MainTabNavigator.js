0d51169c04d2b7f4763bdc53b69db912ab47852b
ployv <>
2019-03-30 12:46:56 +0700

    yeah

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import { createAppContainer, createStackNavigator } from "react-navigation";
 
 import HomeScreen from "../screens/HomeScreen";
+import RegisterEmployers from "../screens/RegisterEmployers";
+import RegisterPWDs from "../screens/RegisterPWDs";
+import LoginEmployers from "../screens/LoginEmployers";
+import LoginPWDs from "../screens/LoginPWDs";
 
 export const Navigator = new createStackNavigator(
   {
-    HomeScreen: {screen: HomeScreen}
+    HomeScreen: {screen: HomeScreen},
+    RegisterEmployers: {screen: RegisterEmployers},
+    RegitsterPWDs: {screen: RegisterPWDs},
+    LoginEmployers: {screen: LoginEmployers},
+    LoginPWDs: {screen: LoginPWDs}
   },
   {
     initialRouteName: "HomeScreen"
